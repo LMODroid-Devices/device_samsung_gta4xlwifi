e8db7e0d6bf6e45a301c58e0c1228a0a8a28f85d
LMO GerritBot <gerrit@libremobileos.com>
2025-09-07 12:54:34 +0000

    Adapt to LMODroid by device-add script

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_gta4xlwifi.mk
+    $(LOCAL_DIR)/lmodroid_gta4xlwifi.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_gta4xlwifi-eng \
-    lineage_gta4xlwifi-user \
-    lineage_gta4xlwifi-userdebug
+    lmodroid_gta4xlwifi-eng \
+    lmodroid_gta4xlwifi-user \
+    lmodroid_gta4xlwifi-userdebug
